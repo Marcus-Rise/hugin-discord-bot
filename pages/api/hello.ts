4de29584efb81fd97f7a1b76a035f9de3dbe8363
Marcus-Rise <ilykonst95@gmail.com>
2021-09-17 10:43:46 +0400

    init

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,69 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import {NextApiHandler} from "next";
+import {InteractionResponseType, InteractionType, verifyKey} from "discord-interactions";
+import getRawBody from "raw-body";
 
-type Data = {
-  name: string
-}
+const INVITE_COMMAND = {
+    name: "Invite",
+    description: "Get an invite link to add the bot to your server",
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const APPLICATION_ID: string = process.env.APPLICATION_ID || "";
+const PUBLIC_KEY: string = process.env.PUBLIC_KEY || "";
+
+const INVITE_URL = `https://discord.com/oauth2/authorize?client_id=${APPLICATION_ID}&scope=applications.commands`;
+
+const Handler: NextApiHandler = async (request, response) => {
+    // Only respond to POST requests
+    if (request.method === "POST") {
+        // Verify the request
+        const signature = String(request.headers["x-signature-ed25519"]);
+        const timestamp = String(request.headers["x-signature-timestamp"]);
+        const rawBody = await getRawBody(request);
+
+        const isValidRequest = verifyKey(
+            rawBody,
+            signature,
+            timestamp,
+            PUBLIC_KEY,
+        );
+
+        if (!isValidRequest) {
+            console.error("Invalid Request");
+            return response.status(401).send({ error: "Bad request signature " });
+        }
+
+        // Handle the request
+        const message = request.body;
+
+        // Handle PINGs from Discord
+        if (message.type === InteractionType.PING) {
+            console.log("Handling Ping request");
+            response.send({
+                type: InteractionResponseType.PONG,
+            });
+        } else if (message.type === InteractionType.APPLICATION_COMMAND) {
+            // Handle our Slash Commands
+            switch (message.data.name.toLowerCase()) {
+                case INVITE_COMMAND.name.toLowerCase():
+                    response.status(200).send({
+                        type: 4,
+                        data: {
+                            content: INVITE_URL,
+                            flags: 64,
+                        },
+                    });
+                    console.log("Invite request");
+                    break;
+                default:
+                    console.error("Unknown Command");
+                    response.status(400).send({ error: "Unknown Type" });
+                    break;
+            }
+        } else {
+            console.error("Unknown Type");
+            response.status(400).send({ error: "Unknown Type" });
+        }
+    }
+};
+
+export default Handler;
